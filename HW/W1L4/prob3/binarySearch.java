3e9dc136414f9d34be339ffe5e9dec3ffa5684f9
eegii <eegii@172.19.141.151>
2019-09-15 14:39:23 -0500

    W2L6

--- HW/W1L4/prob3/binarySearch.java
+++ HW/W1L4/prob3/binarySearch.java
@@ -24,7 +24,10 @@ public class binarySearch {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+		//int tar = 3;
+		
+		int[] arr = {2,3};
 		int tar = 3;
 
 		int result = bSearch(arr, tar);
